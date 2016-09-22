4013a77f0a91f8651a27492755fd687f3a6e20e2
Neil Advani <neil.advani@vanderbilt.edu>
2016-09-22 12:24:45 -0500

    ex6

---
+++ Solutions/Ex6/src/main/java/edu/vanderbilt/spring/Video.java
@@ -0,0 +1,50 @@
+package edu.vanderbilt.spring;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Video {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String genre;
+	private String URL;
+	
+	public String getURL() {
+		return URL;
+	}
+	
+	public void setURL(String URL) {
+		this.URL = URL;
+	}
+	
+	public String getGenre() {
+		return genre;
+	}
+	
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
